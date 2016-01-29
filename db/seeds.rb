209ffe1a28f3e5109259f5ce98ea3e0462f8af94
Justin Thomas <jthomas0209@gmail.com>
2016-01-29 09:05:28 -0500

    Spec issue? Need help.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Mike")
+Song.create(title: "Jones", artist_id: 1)
+Song.create(title: "What?", artist_id: 1)
