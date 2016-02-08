99c896638d8cd7c06b7892d7dbf62d9201acf597
dannyd4315 <dannyd4315@gmail.com>
2016-02-09 10:41:07 +1100

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-end
+
+  def artist_and_title
+    "#{self.artist.name} - #{self.title}"
+  end 
+
+  def artist_name
+    self.artist.name
+  end 
+end  
