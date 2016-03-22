4bbd5dfffa6ec569bcfaf3d6f1702a370d0f4018
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-22 22:06:38 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,15 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @songs = Song.all
   end
 
   def show
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -19,11 +23,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
 
     @song.update(song_params)
 
@@ -35,13 +37,16 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
 
   private
+  
+  def set_song 
+    @song = Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
