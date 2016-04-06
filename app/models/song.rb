fd920fb70fc17f982c383283ff981ba756722295
Lady Codington <myemailforcoding@gmail.com>
2016-04-06 10:50:24 -0400

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_and_title_slug
+    artist_name + ' - ' + self.title
+  end
+
+  def artist_name
+    artist.name
+  end
+
 end
