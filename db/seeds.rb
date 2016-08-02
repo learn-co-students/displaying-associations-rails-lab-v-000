89bef30ae01c17f42cecae53c7e7fdc384f2bf81
mcclains2003 <mcclains2003@gmail.com>
2016-08-02 12:54:40 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist = Artist.create!(name: "Mumford and Sons")
+artist.songs.create!(title: "Hopeless Wanderer")
+artist.songs.create!(title: "I Will Wait")
+
+new_artist = Artist.create!(name: "Taylor Swift")
+new_artist.songs.create!(title: "Shake it Off")
+new_artist.songs.create!(title: "Bad Blood")
