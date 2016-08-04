3244d97049d125883c4f24d45861e3cf8712b6aa
CodeByLine <yleventhal@ymail.com>
2016-08-04 11:54:25 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,21 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
   end
 
   def create
     @song = Song.new(song_params)
 
     if @song.save
+
       redirect_to @song
     else
       render :new
@@ -24,15 +28,16 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-
-    @song.update(song_params)
+    @song.update(params[:id])
 
     if @song.save
+      # @artist.songs << @song
       redirect_to @song
     else
       render :edit
     end
   end
+
 
   def destroy
     @song = Song.find(params[:id])
@@ -41,10 +46,10 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+
   private
 
   def song_params
     params.require(:song).permit(:title)
   end
 end
-
