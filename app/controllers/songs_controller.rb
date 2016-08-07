c6f735c2e102e5c4fbececdd6ea9e61f7001bc49
Joe <jsbenning4@gmail.com>
2016-08-07 00:43:46 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,18 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params)
+    @song = Song.new
 
     if @song.save
       redirect_to @song
