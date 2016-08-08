3be79746e951cda35a6e511999fda87c2d117461
arturusfury <bri.reynolds@gmail.com>
2016-08-08 17:56:23 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,11 @@
+# ArtistsController
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    artist.name
+  end
+
+  def to_s
+    "#{artist_name} - #{title}"
+  end
 end
