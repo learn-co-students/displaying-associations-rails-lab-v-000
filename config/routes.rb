c5d6f711a2f990d1def8a3dadbd9c432157d733a
William Jones <enquiries@forestfed.com>
2016-10-20 20:26:56 +0000

    displaying associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'artists/show'
+
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :artists
+
   resources :artists, :songs
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+cure = Artist.create!(name: "The Cure")
+cure.songs.create!(title: "Boys Dont Cry")
+cure.songs.create!(title: "Close To Me")
+
+smiths = Artist.create!(name: "The Smiths")
+smiths.songs.create!(title: "Panic")
+smiths.songs.create!(title: "Some Girls Are Bigger Than Others")
+
+stranglers = Artist.create!(name: "The Stranglers")
+stranglers.songs.create!(title: "Golden Brown")
+stranglers.songs.create!(title: "Peaches")
