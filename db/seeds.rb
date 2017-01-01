f8a8b760f5b6a34411d8c462a0693edd6b37cedd
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2016-12-31 23:06:33 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Abe Fridman")
+Artist.create(name: "Zalmen Lipsker")
