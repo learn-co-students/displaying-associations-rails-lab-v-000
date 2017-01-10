caa00561224e8aa12db901f6962422cb3e5c792b
msosborne3 <msosborne@ymail.com>
2017-01-09 21:13:46 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Fall Out Boy")
+Artist.create(name: "Twenty One Pilots")
+
+Song.create(title: "Immortals", artist_id: Artist.find_by_name("Fall Out Boy").id)
+Song.create(title: "Centuries", artist_id: Artist.find_by_name("Fall Out Boy").id)
+
+Song.create(title: "Lane Boy", artist_id: Artist.find_by_name("Twenty One Pilots").id)
+Song.create(title: "Message Man", artist_id: Artist.find_by_name("Twenty One Pilots").id)
