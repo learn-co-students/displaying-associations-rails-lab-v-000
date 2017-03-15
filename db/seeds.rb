61a3d01c32474976eacfc2ff29ae473783aef5fb
Kevin Adkins <kevinladkins@gmail.com>
2017-03-15 17:20:11 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+the_cure = Artist.create!(name: "The Cure")
+the_cure.songs.create!(title: "Disintegration")
+the_cure.songs.create!(title: "The Lovecats")
+the_cure.songs.create!(title: "Shake Dog Shake")
+the_cure.songs.create!(title: "Birdmad Girl")
+
+sleaford_mods = Artist.create!(name: "Sleaford Mods")
+sleaford_mods.songs.create!(title: "Live Tonight")
+sleaford_mods.songs.create!(title: "Messy Anywhere")
+sleaford_mods.songs.create!(title: "Pubic Hair Ltd.")
+sleaford_mods.songs.create!(title: "Army Nights")
