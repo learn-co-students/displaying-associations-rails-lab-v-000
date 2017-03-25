0ce3bf26330f40627b83afa1de89c744e48d9fe4
John Pfingst <jpfingst@outlook.com>
2017-03-25 16:19:51 +0000

    rails AR/mixed models

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,8 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist!, only: [:show, :edit, :update, :destroy]
+
   def index
+    @artists = Artist.all
   end
 
   def show
@@ -20,15 +23,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
+    if @artist.update(artist_params)
       redirect_to @artist
     else
       render :edit
@@ -36,7 +34,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
@@ -46,5 +43,11 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  private
+
+  def set_artist!
+    @artist = Artist.find(params[:id])
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  validates :title,  presence: true
+  validates :artist, presence: true
+
+  def artist_name
+    artist.name
+  end
 end
