efbca4f51aec209d13a0e6308b0eada4466a51f2
kkashuda <kkashuda@gmail.com>
2017-04-27 21:02:58 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,14 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.all.find(params[:id])
   end
 
   def new
+    @song = Song.new
   end
 
   def create
