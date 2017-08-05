440ad796037c342d5d90450aeae3eb79aa033bb1
Spencer Evans <spencevans@gmail.com>
2017-08-05 00:22:48 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,18 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :delete]
+
   def index
+    @songs = Song.all
   end
 
   def show
   end
 
   def new
+    @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -19,15 +21,11 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+
   end
 
   def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
+    if @song.update(song_params)
       redirect_to @song
     else
       render :edit
@@ -35,7 +33,6 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -43,8 +40,11 @@ class SongsController < ApplicationController
 
   private
 
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
   def song_params
     params.require(:song).permit(:title)
   end
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+@artist = Artist.create!(name: "Daft Punk")
+@grid = @artist.songs.create!(title: "The Grid")
+@voyager = @artist.songs.create!(title: "Voyager")
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
