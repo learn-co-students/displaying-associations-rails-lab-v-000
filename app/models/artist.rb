18eaa9f61e54542d20bbe50446b7e26358a4a51b
leog7one <lguzman@me.com>
2017-09-19 13:56:03 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+	has_many :songs
+
+# define song count method in the model
+#These methods are meant to protect the views from complexity that belongs in the model.
+	def song_count
+		self.songs.size
+	end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+#define artist name method
+#These methods are meant to protect the views from complexity that belongs in the model.
+  def artist_name
+  	self.artist.name
+  end
+
 end
