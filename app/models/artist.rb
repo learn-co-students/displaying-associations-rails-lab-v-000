6b377f4e33a732179f8af7b8bb1f1ffa1288c0e3
davidtspf <david.tannenbaum@gmail.com>
2017-10-29 14:43:35 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,15 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    @artists = Artist.all
+    count = 0
+    @artists.each do |artist|
+      artist.songs.each do |song|
+        count += 1
+      end
+    end
+    count
+  end
+
 end
