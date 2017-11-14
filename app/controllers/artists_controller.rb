ec217ff1926d7a30f91fc78d21976e755d75ef78
johanesteves <jsesteves91@gmail.com>
2017-11-13 21:57:04 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -42,9 +44,17 @@ class ArtistsController < ApplicationController
     redirect_to artists_path
   end
 
+  def song_count
+    @s_count = self.songs.count
+  end
+
   private
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def song_count
+    @s_count = self.songs.count
   end
 end
