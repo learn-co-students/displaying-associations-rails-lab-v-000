7c7741c64543757a57643f772b865de3939dbbae
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-19 10:50:40 -0500

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    set_artist
   end
 
   def new
@@ -46,5 +48,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 end
