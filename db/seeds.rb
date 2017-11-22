e688cc7cdf5a2b2ba3ade4687a0b68f99a4ec43d
Robin Pahlman <robin.pahlman@gmail.com>
2017-11-22 11:07:37 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bjm = Artist.create(name: "Brian Jonestown Massacre")
+can = Artist.create(name: "Can")
+neu = Artist.create(name: "Neu!")
+bjm.songs.create(title: "Viholliseni Maalla")
+bjm.songs.create(title: "Panic In Babylon")
+can.songs.create(title: "Vitamin C")
+can.songs.create(title: "Future Days")
+can.songs.create(title: "Mushroom")
+neu.songs.create(title: "Hallogallo")
+neu.songs.create(title: "Fur Immer")
+neu.songs.create(title: "Super 78")
+neu.songs.create(title: "Lieber Honig")
+
+
+
