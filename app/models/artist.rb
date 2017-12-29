93e1c3e00ccbec4a8aa74d78b3f1462ffe67d2c6
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-29 09:41:46 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ActiveRecord::Base
+
+  has_many :songs
+
+  def song_count
+    #ActionController::Base.helpers.pluralize(self.songs.count, 'song')
+    self.songs.count
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def format_artist_name_title
+    "#{self.artist.name} - #{self.title}"
+  end
+
+  def artist_name
+    self.artist.name
+  end
 end
