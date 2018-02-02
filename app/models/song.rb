ef4ec9f8efd6e4682ce03238562212c83d0d1e95
Jess Mitchell <jessicamitch42@gmail.com>
2018-02-02 00:49:42 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def create_artist_title_format
+    "#{self.artist_name} - #{self.title}"
+  end
+
+  def artist_name
+    self.artist.name
+  end
 end
