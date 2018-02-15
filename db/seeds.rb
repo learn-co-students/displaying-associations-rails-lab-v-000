b224497ff4408838867ac2557c9e3ed26f77ce09
Adam <github email address>
2018-02-15 02:25:29 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+drake = Artist.create(:name => "Drake")
+taylor = Artist.create(:name => "Taylor Swift")
+daft_punk = Artist.create(:name => "Daft Punk")
+
+Song.create(:title => "Drake's best song ever", :artist_id => drake.id)
+Song.create(:title => "Drake's worst song ever", :artist_id => drake.id)
+Song.create(:title => "Taylor's best song ever", :artist_id => taylor.id)
+Song.create(:title => "Taylor's worst song ever", :artist_id => taylor.id)
+Song.create(:title => "The Grid", :artist_id => daft_punk.id)
