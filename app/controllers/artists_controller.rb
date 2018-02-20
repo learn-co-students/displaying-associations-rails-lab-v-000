e44aba970a58647b06d28fc311bbed8b26ec9b3c
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-02-20 15:05:07 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,11 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: :show
+  
   def index
     @artists = Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -45,7 +46,11 @@ class ArtistsController < ApplicationController
   end
 
   private
-
+  
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+  
   def artist_params
     params.require(:artist).permit(:name)
   end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,11 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: :show
+  
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def new
@@ -44,6 +45,10 @@ class SongsController < ApplicationController
   end
 
   private
+
+  def set_song
+    @song =  Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
