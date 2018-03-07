08add5126919e23e0f733262ae66e28dcba40239
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-03-07 21:15:36 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,16 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+     @song = Song.find(params[:id])
+     @artists = Artist.all
   end
 
   def new
+    @song = Song.new
+    @artists = Artist.all
   end
 
   def create
@@ -20,6 +25,7 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find(params[:id])
+    @artists = Artist.all
   end
 
   def update
