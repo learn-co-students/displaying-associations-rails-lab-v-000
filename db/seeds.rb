93dbd1574db729ffde02ba2800c7e6522ca88f21
EC2 Default User <ec2-user@ip-172-31-12-72.us-west-2.compute.internal>
2018-03-11 19:03:55 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artists = Artist.create!([{ name: 'Bon Jovi' }, { name: 'Coldplay' }, { name: 'Journey' }])
+bon_jovi = Artist.first
+livin_prayer= bon_jovi.songs.create!(title: "Livin on a Prayer")
+
+coldplay = Artist.find_by(name: "Coldplay")
+clocks = coldplay.songs.create!(title: "Clocks")
+
+journey = Artist.find_by(name: "Journey")
+dont_stop = journey.songs.create!(title: "Don't Stop Believin")
+
+
+
+
