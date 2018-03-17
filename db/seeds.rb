436db8afc972e07e541d7348449cbb3329f821d9
jacobsilver2 <jacobsilver2@mac.com>
2018-03-17 11:21:57 -0400

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist = Artist.create!(name: "Bob M")
+artist.songs.create!(title: "Trenchtown Rock")
+artist.songs.create!(title: "Three Little Birds")
