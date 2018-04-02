666b7618603620cb4463772464dfae3cf301b603
ingridwong0715 <ingrid830715@gmail.com>
2018-04-01 21:59:04 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def display_format
+    self.artist_name + " - " + self.title
+  end
 end
