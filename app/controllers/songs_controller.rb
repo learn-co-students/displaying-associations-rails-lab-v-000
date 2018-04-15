f3859d655bee3efb710f6dd4afeeb38423e393e6
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-15 15:19:00 +0100

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,18 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: %i[show edit update destroy]
+
   def index
+    @songs = Song.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @song = Song.new
   end
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -18,16 +20,10 @@ class SongsController < ApplicationController
     end
   end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
+    if @song.update(song_params)
       redirect_to @song
     else
       render :edit
@@ -35,16 +31,18 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
-    flash[:notice] = "Song deleted."
+    flash[:notice] = 'Song deleted.'
     redirect_to songs_path
   end
 
   private
 
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
   def song_params
     params.require(:song).permit(:title)
   end
 end
-
