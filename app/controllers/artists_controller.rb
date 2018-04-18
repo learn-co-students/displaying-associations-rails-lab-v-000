25ffca975d2bad39af2e5ab294fc0f7d123386c6
shravnk <brendan.ode4@gmail.com>
2018-04-18 12:44:12 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,8 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: [:show, :edit, :update, :destroy]
+
   def index
+    @artists = Artist.all
   end
 
   def show
@@ -20,12 +23,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
+   
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
 
     if @artist.save
@@ -46,5 +47,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def find_artist
+    @artist = Artist.find(params[:id])
   end
 end
