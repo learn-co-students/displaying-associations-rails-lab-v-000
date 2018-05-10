f05f00d9db98b0de4efc622c539cfddadeb0133f
Naomi Dennis <njdennis94@gmail.com>
2018-05-10 21:49:08 +0000

    All Model Specs Passed

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.nil? ? self.artist.name : 'None'
+    self.artist.nil? ? 'None' : self.artist.name
   end
 end
