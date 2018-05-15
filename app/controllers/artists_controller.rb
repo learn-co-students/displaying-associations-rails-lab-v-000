585e1aa5f123ed288db4d2f173a04f4559277fb2
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-05-15 12:27:21 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,12 @@
 class ArtistsController < ApplicationController
+
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = artist
+    @songs = @artist.songs
   end
 
   def new
@@ -46,5 +50,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def artist
+    Artist.find(params[:id])
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+
+    def song_count
+      self.songs.count
+    end
+
+    # def song_count_pluralized
+    #   songs = self.songs.count
+    #   songs == 1 ? "#{songs}" + " " "song" : "#{songs}" + " " "songs"
+    # end
 end
