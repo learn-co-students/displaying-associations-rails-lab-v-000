7ab8fe321936631c6bd269a25683612af7d10c9e
github username <github email address>
2018-05-24 18:27:09 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,7 +16,7 @@ class ArtistsController < ApplicationController
 
     if @artist.save
       @artist.save
-      redirect_to @artist
+      redirect_to artist_path(@artist)
     else
       render :new
     end
@@ -32,7 +32,7 @@ class ArtistsController < ApplicationController
     @artist.update(artist_params)
 
     if @artist.save
-      redirect_to @artist
+      redirect_to artist_path(@artist)
     else
       render :edit
     end
