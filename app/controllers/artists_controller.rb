449c909c74af305bf1466c2cb0f8b0dc4a3e8e19
github username <github email address>
2018-05-24 17:22:08 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -15,6 +15,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.new(artist_params)
 
     if @artist.save
+      @artist.save
       redirect_to @artist
     else
       render :new
@@ -37,12 +38,12 @@ class ArtistsController < ApplicationController
     end
   end
 
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
+  # def destroy
+  #   @artist = Artist.find(params[:id])
+  #   @artist.destroy
+  #   flash[:notice] = "Artist deleted."
+  #   redirect_to artists_path
+  # end
 
   private
 
