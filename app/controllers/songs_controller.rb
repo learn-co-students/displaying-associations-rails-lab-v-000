bdda072d8a8ba3acde5edf9673c333ede7200a22
Kim Gonzales <Kimgo589@gmail.com>
2018-05-25 16:00:53 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,15 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
+    @songs = Song.all
   end
 
   def show
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -19,15 +23,10 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
+     if @song.update(song_params)
       redirect_to @song
     else
       render :edit
@@ -35,13 +34,16 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
 
   private
+  
+  def set_song
+    @song = Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+taylor = Artist.create!(name: "Taylor Swift")
+taylor.songs.create!(title: "Trouble")
+taylor.songs.create!(title: "Red")
+taylor.songs.create!(title: "Style")
+
+adele = Artist.create!(name: "Adele")
+adele.songs.create!(title: "Hello")
+adele.songs.create!(title: "Best For Last")
+adele.songs.create!(title: "Send My Love")
+adele.songs.create!(title: "Rolling In The Deep")
+
+drake = Artist.create!(name: "Drake")
+drake.songs.create!(title: "Passionfruit")
+drake.songs.create!(title: "Take Care")
+drake.songs.create!(title: "One Dance")
