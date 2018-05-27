a8396b7d98edbf85f124f008e7d911449cfda80d
Kaitlin Kelly <kk504408@gmail.com>
2018-05-27 21:28:56 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,16 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
 
-  def song_count
+  def song_count_display
     if self.songs.count == 1
       "#{self.songs.count} song"
     else
       "#{self.songs.count} songs"
     end
+  end
+
+  def song_count
+    self.songs.count
   end
 
 end
