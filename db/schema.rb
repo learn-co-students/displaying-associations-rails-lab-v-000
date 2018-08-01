232d6948c46dc56f2978c8e14e86140eb653ae6c
swgoodman <spencerwgoodman@icloud.com>
2018-08-01 10:27:33 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108015408) do
+ActiveRecord::Schema.define(version: 20180801164420) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20160108015408) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "artist_id"
+    t.string   "artist_name"
   end
 
 end
