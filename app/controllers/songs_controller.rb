fe974366206139c7c76e6ce312797bd904ac2f0c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-04 19:17:37 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,14 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    find_song
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -19,12 +22,11 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    find_song
   end
 
   def update
-    @song = Song.find(params[:id])
-
+    find_song
     @song.update(song_params)
 
     if @song.save
@@ -35,7 +37,7 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
+    find_song 
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -46,5 +48,9 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title)
   end
-end
 
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
+end
