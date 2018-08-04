7534675f4a598ddbd333d084f34b9e4d2f412143
Elyse Klova <elyse.klova@gmail.com>
2018-08-04 17:20:10 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ricky = Artist.create(name: "Ricky Martin")
+ricky.songs.create(title: "Livin' La Vida Loca")
+ricky.songs.create(title: "She Bangs")
+
+
+nsync = Artist.create(name: "*NSYNC")
+nsync.songs.create(title: "Tearin' Up My Heart")
+nsync.songs.create(title: "Bye Bye Bye")
+nsync.songs.create(title: "It's Gonna Be Me")
+nsync.songs.create(title: "(God Must Have Spent) A Little More Time On You")
