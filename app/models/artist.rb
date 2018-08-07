9125112aa96e240d1357a65ebd39f66cac0f3b5b
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-07 18:53:50 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,17 @@
 class Artist < ActiveRecord::Base
+  
+  has_many :songs
+  
+  before_create :make_title_case
+  
+  def make_title_case
+    self.name = self.name.titlecase
+  end
+  
+  def song_count
+    self.songs.count
+  end
+  
+  
+  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  
+  before_create :make_title_case
+  
+  def make_title_case
+    self.title = self.title.titlecase
+  end
+  
+  def artist_name
+    self.artist.name
+  end
 end
