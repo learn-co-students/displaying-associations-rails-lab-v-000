acf56117086f5cf272c805ec5eeb867132cd1530
Cl3arglass <haltaffer@gmail.com>
2018-08-13 15:08:03 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -42,7 +44,10 @@ class ArtistsController < ApplicationController
     redirect_to artists_path
   end
 
+
+
   private
+
 
   def artist_params
     params.require(:artist).permit(:name)
