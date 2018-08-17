0849bc61676f80daedd83283df812fbcb27ad891
Michael Kang <michaelkang85@gmail.com>
2018-08-16 20:34:07 -0400

    finished lab

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,24 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  def song_count
+    self.songs.count
+  end
+
+  private
+
+ def is_title_case
+   if name.split.any?{|w|w[0].upcase != w[0]}
+     errors.add(:name, "Name must be in title case")
+   end
+ end
+
+ def make_title_case
+   self.name = self.name.titlecase
+ end
+
+
 end
