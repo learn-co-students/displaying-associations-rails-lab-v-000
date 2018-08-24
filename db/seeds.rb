34f5d8d6e233bff10a1bba00df642ba395f38f37
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-08-24 15:35:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# tay = Artist.create(name: "Taylor Swift")
+#
+# stay = Song.create(title: "Stay", artist_id: tay.id)
+#
+# jb = Artist.create(name: "Justin Bieber")
+#
+# baby = Song.create(title: "Baby", artist_id: jb.id)
