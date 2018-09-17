81dc3532a2763b763eeb492a529008c4dc3caa41
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-09-17 04:56:56 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artists = Artist.create([
+  {name: 'Drake'}, {name: 'Cardi B'}, {name: 'Sia'}
+  ])
+
+drakes_songs = Song.create([
+  {title: 'Hotline Bling'}, {title: "God's Plan"}
+  ])
+
+cardis_songs = Song.create([
+  {title: 'Bodak Yellow'}, {title: 'I Like It'}
+  ])
+
+sias_songs = Song.create([
+  {title: 'Chandelier'}, {title: 'Cheap Thrills'}
+  ])
+
+artists.first.songs << drakes_songs
+artists[1].songs << cardis_songs
+artists[2].songs << sias_songs
