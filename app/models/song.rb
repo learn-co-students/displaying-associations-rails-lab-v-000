dd10de2dd6812223742eac80019ffe9af42c55c2
epic0448 <samuelogundiran@Yahoo.com>
2018-09-19 06:52:58 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    # binding.pry  Always binding. pry.....  at this locations to get answers.
+    # Always binding. pry.....  at this locations to get answers.
     self.artist.name
   end
   
