d29183124b0b76835f02a830916873f3420cd2bb
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-10-04 15:43:13 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  def artist_name
+    self.artist.name
+  end
+
+  private
+
+  def is_title_case
+    if title.split.any? {|w| w[0] != w[0].upcase}
+      errors.add(:name, "Title must be Title Case")
+    end
+  end
+
+  def make_title_case
+    self.title = self.title.titlecase
+  end
 end
