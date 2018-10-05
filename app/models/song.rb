314a69cc392ffb1e93172ef51f16460c45177f34
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-05 01:40:50 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  
+  def with_artist
+    "#{self.artist_name} - #{self.title}"
+  end 
+  
+  def artist_name 
+    self.artist.name 
+  end 
+  
 end
