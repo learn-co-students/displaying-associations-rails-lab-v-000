8c5a743f688c614b3c5559c57150171891994ed7
Madeline-Stark <madeline.stark1@gmail.com>
2018-11-07 14:17:29 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    self.songs.count
+  end
+
+  def pluralize_song
+    'song'.pluralize(self.song_count) #will pluralize song if more than one
+  end
+
 end
