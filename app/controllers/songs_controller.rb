6af52e326b16190dffea2195d59a26a4fc99ea24
github username <github email address>
2018-12-06 11:46:49 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,14 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    
   end
 
   def create
