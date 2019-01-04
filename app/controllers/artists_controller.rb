7a1da8ca7f3a0d7a1eeeba045d98f5431a3a7645
Rebecca <rtong@buffalo.edu>
2019-01-03 21:25:16 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,16 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find_by(params[:id])
   end
 
   def new
     @artist = Artist.new
   end
+
 
   def create
     @artist = Artist.new(artist_params)
