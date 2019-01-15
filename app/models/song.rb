c698761e48a7a42dd9549ef2d2da2d34bef674c1
github username <github email address>
2019-01-15 12:01:16 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  #instance method to format the songs on index page
+  # Artist Name - Song Title
+  def artist_name
+    self.artist.name 
+  end
 end
