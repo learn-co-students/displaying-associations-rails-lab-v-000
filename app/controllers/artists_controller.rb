0dd051898087615107007ce73bd9735a717851d5
VincePimentel <contact@vincep.me>
2019-02-03 19:47:47 -0800

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,8 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: %i[show edit update destroy]
+
   def index
+    @artists = Artist.all
   end
 
   def show
@@ -20,12 +23,9 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
 
     if @artist.save
@@ -36,13 +36,16 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
   end
 
   private
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name)
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,15 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: %i[show edit update destroy]
+
   def index
+    @songs = Song.all
   end
 
   def show
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -19,12 +23,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
-
     @song.update(song_params)
 
     if @song.save
@@ -35,13 +36,16 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
 
   private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
