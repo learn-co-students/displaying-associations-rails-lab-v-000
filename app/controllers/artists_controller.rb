ba77400f5660a7d0e49203d902244158532ea61d
Jonathan Luk <jluk87@gmail.com>
2019-04-06 23:22:37 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,9 @@
 class ArtistsController < ApplicationController
+
+  before_action :set_artist, only: [:show, :edit, :update, :destry]
+
   def index
+    @artists = Artist.all
   end
 
   def show
@@ -20,11 +24,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
+
   end
 
   def update
-    @artist = Artist.find(params[:id])
 
     @artist.update(artist_params)
 
@@ -36,13 +39,16 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name)
