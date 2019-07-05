15bb90d5cf4d42c190e0bd5b18218d4df1d84969
Mohammed Noori <kmanoorib@gmail.com>
2019-07-05 19:33:33 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  
+  # Law of Demeter example
+  def song_count
+    self.songs.count # maybe can replace .count with .size or .length
+  end
+  
 end
