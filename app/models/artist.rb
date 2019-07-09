f8cd631597c8c23c7c74725c595cbd8d16802d0a
github username <github email address>
2019-07-09 13:19:03 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        self.songs.count
+        #simplicity AKA the Law of Demeter AKA one dot rule - Beth Urban 
+    end
 end
