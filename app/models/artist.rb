d809c94d839e525cd026f08a44e9be2ae98254bc
david-forer <david.j.forer@gmail.com>
2019-09-11 11:57:57 +0900

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+
+    def song_count
+        self.songs.count
+    end
+
+    def index_display_name
+        self.name + " - " + self.song_count.to_s + " songs"
+    end
 end
