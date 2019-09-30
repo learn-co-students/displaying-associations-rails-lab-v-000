b9f820794d335b9001253a78b5af6566a6dd691c
Amil <github email address>
2019-09-30 15:07:49 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist = Artist.create!(name: "Eminem")
+song = Song.create!(title: "Lose Yourself", artist_id: 1)
+song = Song.create!(title: "Without Me", artist_id: 1)
