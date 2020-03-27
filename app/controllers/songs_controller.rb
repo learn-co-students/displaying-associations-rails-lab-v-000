005d73fd1951d4594fb3b42038004252cc921088
Diapause <diapause@protonmail.com>
2020-03-26 19:19:52 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,11 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: [:show,:edit,:update,:destroy]
   def index
+    @songs = Song.all
   end
 
   def show
+    find_song
   end
 
   def new
@@ -10,10 +13,10 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params)
+    @song = Song.create(song_params)
     @song.artist = Artist.find_or_create_by(artist_params)
 
-    if @song.save
+    if @song.valid?
       redirect_to @song
     else
       render :new
@@ -21,15 +24,14 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    find_song
   end
 
   def update
-    @song = Song.find(params[:id])
-
+    find_song
     @song.update(song_params)
 
-    if @song.save
+    if @song.valid?
       redirect_to @song
     else
       render :edit
@@ -37,7 +39,7 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
+    find_song
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -51,5 +53,9 @@ class SongsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def find_song
+    @song = Song.find(params[:id])
   end
 end
